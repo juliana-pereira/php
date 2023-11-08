23ae3cfef779f5714ff20e352f68f5e15cf1a77f
Juliana Pereira <145492411+juliana-pereira@users.noreply.github.com>
2023-11-08 20:29:42 -0300

    Add files via upload

---
+++ aula02/exp03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp03.css">
+</head>
+<body>
+
+<p> elemento p </p>
+<h1>elemento h1</h1>
+<h2>elemento h2</h2>
+<p id="elemento2"> novo elemento com id</p>
+
+<div class="div1">div 1</div>
+<div class="div1">div 1</div>
+<p class="div1">paragrafo</p>
+<h1 class="div1">html texto</h1>
+    
+</body>
+</html>
---
+++ aula02/exp04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp04.css">
+    <style> 
+    p{ background-color: aliceblue;}
+    .center{background-color: aquamarine;}
+    p.center{background-color: yellow;}
+</style>
+
+    </head>
+<body>
+    <p> estilo no arquivo </p>
+    <p> estilo no arquivo </p>
+    <p id="estilo">estilo no arquivo</p>
+    <p style="background-color: beige;">estilo da linha</p>
+
+    <p class="center">exemplo classe</p>
+    <h1 class="center">exemplo com h1<h1>
+</html>
+
