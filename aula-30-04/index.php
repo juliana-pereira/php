644d31ee2996b1235b90f009b1bd787ae68e1cf0
Juliana Pereira <145492411+juliana-pereira@users.noreply.github.com>
2024-04-30 16:15:43 -0300

    Add files via upload

---
+++ aula-30-04/index.php
@@ -0,0 +1,67 @@
+<?php
+
+echo "Exemplo 1";
+echo "<br>";
+$VAR = array(2,5,7,9,11);
+echo $VAR[0];
+echo "<br>";
+echo $VAR[1];
+echo "<br>";
+echo $VAR[2];
+echo "<br>";
+echo $VAR[3];
+echo "<br>";
+echo $VAR[4];
+echo "<br>";
+echo "<br>";
+
+echo "Exemplo 2";
+echo "<br>";
+$var = array("Antônio", "João", "Zeca");
+echo $var[0];
+echo "<br>";
+echo $var[1];
+echo "<br>";
+echo $var[2];
+echo "<br>";
+echo "<br>";
+
+echo "Exemplo 3";
+echo "<br>";
+$var = array("Windows", 10, 2024, "v1.2021");
+echo $var[0];
+echo "<br>";
+echo $var[1];
+echo "<br>";
+echo $var[2];
+echo "<br>";
+echo $var[3];
+echo "<br>";
+echo "<br>";
+
+echo "Exemplo 4";
+echo "<br>";
+$var = array("Ítalo"=>30, "Hubert"=>20, "Luna"=>18);
+echo "a idade é" .$var['Ítalo'];
+echo "<br>";
+echo "a idade é" .$var['Hubert'];
+echo "<br>";
+echo "a idade é" .$var['Luna'];
+echo "<br>";
+echo "<br>";
+
+echo "Exemplo 5";
+echo "<br>";
+$num = array(2=>7, 3=>8, 9=>5);
+$num[8] = 40;
+echo $num[2];
+echo "<br>";
+echo $num[3]=9;
+echo "<br>";
+echo $num[9];
+echo "<br>";
+echo "<br>";
+
+echo "exemplo função count";
+
+?>
